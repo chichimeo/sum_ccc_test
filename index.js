164ef737cc0873b97672cf6ea896322979166124
xyz <desotanalomolatachi@gmail.com>
2024-05-28 14:57:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const areObjects = require('are-objects');
 const areArrays = require('are-arrays');
 
-const sum = (a, b) => a + b
-exports.sum = sum
+/**
+ * Adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The sum of the two numbers.
+ */
+function sum(a, b) {
+    return a + b;
+}
+exports.sum = sum;
 
-const sub = (a, b) => a - b
-exports.sub = sub
+/**
+ * Subtracts two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The difference between the two numbers.
+ */
+function sub(a, b) {
+    return a - b;
+}
+exports.sub = sub;
 
-const mul = (a, b) => a * b
-exports.mul = mul
+/**
+ * Multiplies two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} - The product of the two numbers.
+ */
+function mul(a, b) {
+    return a * b;
+}
+exports.mul = mul;
 
-const div = (a, b) => a / b
-exports.div = div
+/**
+ * Divides two numbers.
+ * @param {number} a - The dividend.
+ * @param {number} b - The divisor.
+ * @returns {number} - The quotient of the two numbers.
+ */
+function div(a, b) {
+    return a / b;
+}
+exports.div = div;
