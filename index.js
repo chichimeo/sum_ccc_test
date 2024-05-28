5854e67e7955be57eb514e4a3f3ea564131ed56a
xyz <desotanalomolatachi@gmail.com>
2024-05-28 15:03:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,28 @@ function div(a, b) {
     return a / b;
 }
 exports.div = div;
+
+/**
+ * Adds two big numbers represented as strings.
+ * @param {string} a - The first big number.
+ * @param {string} b - The second big number.
+ * @returns {string} - The sum of the two big numbers.
+ */
+function addBigNumbers(a, b) {    let result = "";
+    let carry = 0;
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let digitA = i >= 0 ? parseInt(a[i]) : 0;
+        let digitB = j >= 0 ? parseInt(b[j]) : 0;
+        let sum = digitA + digitB + carry;
+        result = (sum % 10) + result;
+        carry = Math.floor(sum / 10);
+        i--;
+        j--;
+    }
+
+    return result;
+}
+exports.addBigNumbers = addBigNumbers;
